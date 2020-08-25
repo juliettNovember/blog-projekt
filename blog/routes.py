4807cad7b7511cc67377b793b1c1f036923f1533
juliettNovember <nowakowska.just@gmail.com>
2020-08-25 18:39:39 +0100

    Drafts

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,17 @@
-from flask import render_template, request
+import functools
+from flask import render_template, request, session, flash, redirect, url_for
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
+
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
+    return check_permissions
 
 def process_entry(entry_id=None):
     if entry_id==None:
@@ -29,7 +39,7 @@ def process_entry(entry_id=None):
             db.session.commit()
         else:
             errors = form.errors
-    return render_template("entry_form.html", form=form, errors=errors)
+    return render_template("base.html", form=form, errors=errors)
 
 @app.route("/")
 def index():
@@ -39,5 +49,47 @@ def index():
 @app.route("/post", defaults={"entry_id": None}, methods=["GET", "POST"])
 
 @app.route("/post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def edit_entry(entry_id):
     return process_entry(entry_id)
+
+@app.route("/drafts/", methods=['GET'])
+@login_required
+def list_drafts():
+    drafts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    return render_template("drafts.html", drafts=drafts)
+
+# @app.route("/delete/<int:entry_id>", methods=["POST"])
+# @login_required
+# def delete_entry(entry_id):
+#     if request.method == 'POST':
+#             if form.validate_on_submit():
+#                 db.session.delete(entry)
+#                 db.session.commit()
+#             else:
+#                 errors = form.errors
+#     return render_template("entry_form.html", form=form, errors=errors)
+
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True 
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
